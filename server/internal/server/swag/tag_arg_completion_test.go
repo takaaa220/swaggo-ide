47469eb512195dd8f6d8649c3128c8fb07f4da66
takaaa220 <taaaaaaaaaa220@gmail.com>
2024-11-23 15:45:18 +0900

    debug

--- server/internal/server/swag/tag_arg_completion_test.go
+++ server/internal/server/swag/tag_arg_completion_test.go
@@ -37,7 +37,7 @@ func TestGetTagArgCompletionItems(t *testing.T) {
 					},
 					{
 						Kind:  protocol.CompletionItemKindKeyword,
-						Label: "query",
+						Label: "qury",
 					},
 					{
 						Kind:  protocol.CompletionItemKindKeyword,
