7caf7776389d3469175101261647055e26dde27f
takaaa220 <taaaaaaaaaa220@gmail.com>
2024-12-23 20:33:54 +0900

    fix the release script

--- release.sh
+++ release.sh
@@ -7,4 +7,8 @@ go build -o ../vscode_client/lsp-server main.go
 cd ../vscode_client
 
 echo "Building and publishing VSCode client"
-pnpm publish
+npm run publish
+
+# if personal access token is expired, you can create a new one and replace the old one.
+# 1. create pat on https://dev.azure.com/<pulisher>/_usersSettings/tokens
+# 2. vsce login <publisher>
